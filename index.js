b21fbb904e625baca6797d8e5c366f5373d82960
Abofazl <a.asghari251@gmail.com>
2021-10-02 01:19:13 +0330

    Improve user age of Java codecs

---
+++ index.js
@@ -0,0 +1,7 @@
+let func=function(){
+    var x=document.getElementById("age").value;
+    document.getElementById("out").innerHTML=x;
+}
+
+
+let y = () => alert('از انجام بازنشانی تنظیمات اطمینان دارید؟')
