9b5a151696e48704c51eee06aad668c6f288b400
Abofazl <a.asghari251@gmail.com>
2021-10-02 11:09:59 +0330

    Add JavaScript code

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let func=function(){
     document.getElementById("out").innerHTML=x;
 }
 let y = () => alert('از انجام بازنشانی تنظیمات اطمینان دارید؟');
+
+function fun() {
+    if(document.getElementById('fname').value==null && document.getElementById('lname').value==null && document.getElementById('fathername').value==null && document.getElementById('nationID').value==null && document.getElementById('email').value==null){
+        alert('تمام فیلدهای قسمت اطلاعات شخص راپرکنید.')
+    }
+}
